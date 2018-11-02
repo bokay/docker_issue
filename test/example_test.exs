040f57611b512febc22029a266c32cc1b9df2aaf
Nicolas Charlery <nicolas@bokay.io>
2018-11-02 15:32:30 -0400

    Add simple plug example with test

--- test/example_test.exs
+++ test/example_test.exs
@@ -1,8 +1,21 @@
 defmodule ExampleTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Example
 
-  test "greets the world" do
-    assert Example.hello() == :world
+  use Plug.Test
+
+  @opts Example.init([])
+
+  test "returns hello world" do
+    # Create a test connection
+    conn = conn(:get, "/hello")
+
+    # Invoke the plug
+    conn = Example.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "Hello world"
   end
 end
